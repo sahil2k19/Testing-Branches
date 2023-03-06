0e65e822acc8b1109c212c4e6494370749c242b7
sahil2k19 <sahilgagan227@gmail.com>
2023-03-06 22:33:28 +0530

    newComponent added

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        sahil is my name
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueJsNewComponent',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
