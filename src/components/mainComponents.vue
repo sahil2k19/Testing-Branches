2d13a8466f9ac2cacd28a049bbd9aa55f77ec1dd
@sahiloas36ty <sahiloas36ty@gmail.com>
2023-03-11 08:31:22 +0530

    this is branch 2 code

---
+++ src/components/mainComponents.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        main componets
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueJsMainComponents',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
